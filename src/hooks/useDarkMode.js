083d6921d49bab76e959899392e674a57f7f2399
kait hunnicutt <katerenehunnicutt@gmail.com>
2021-05-06 16:50:16 -0600

    submit

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import useLocalStorage from './useLocalStorage'
+
+const useDarkMode = () => {
+
+  const [ darkMode, setDarkMode ] = useLocalStorage("dark mode")
+
+  return [darkMode, setDarkMode]
+}
+
+export default useDarkMode
